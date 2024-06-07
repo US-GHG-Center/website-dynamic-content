27142c8e8dcd1176e5fc46a43a4eda6e846a8250
Slesa Adhikari <slesaad@gmail.com>
2024-06-07 16:42:22 -0500

    Print new banner

--- scripts/banner.py
+++ scripts/banner.py
@@ -5,6 +5,7 @@ import os
 files = os.list_dir("_data")
 # Read the output file
 new_banner = json.load(open(f"_data/{files[0]}"))
+print(new_banner)
 
 # Update the existing file
 json.dump(new_banner, open("banner.json", "w"))
