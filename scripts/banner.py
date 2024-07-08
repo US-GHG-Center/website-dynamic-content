7e30ebd5b97774562f281fdd762cf2c3caf63620
Binita <binitagyawali000@gmail.com>
2024-07-08 09:56:36 -0500

    changed on the script

--- scripts/banner.py
+++ scripts/banner.py
@@ -1,18 +1,3 @@
-# import json
-# import os
-#
-# # Get the output file
-# if not os.path.exists("_data"):
-#     os.makedirs("_data")
-#
-# files = os.listdir("_data")
-# print(files)
-# # Read the output file
-# new_banner = json.load(open(f"_data/{files[0]}"))
-# print(new_banner)
-#
-# # Update the existing file
-# json.dump(new_banner, open("banner.json", "w"))
 import json
 import os
 
